af1354c69abf18f9da5abbb0132386829c6b438f
Luan-Marcelino <luanmarcelinocontato@gmail.com>
2025-09-30 22:02:33 -0300

    Mudei o js e apliquei media query

--- script/script.js
+++ script/script.js
@@ -2,15 +2,24 @@ const body = document.querySelector('body');
 const cookie_esquerda = document.querySelector('.cookie-header-esquerda');
 const cookie_direita = document.querySelector('.cookie-header-direita');
 
+
+
 window.addEventListener('scroll', () => {
     const tamanhoY = window.scrollY;
+    const tamanhoX = window.innerWidth;
+    console.log(tamanhoX);
     
-    while(tamanhoY <= 175) {
-        cookie_esquerda.style.right = tamanhoY * 1 + 'px';
-        cookie_direita.style.left = tamanhoY * 1.1 + 'px';
-        break;        
+    if(tamanhoX > 480 && tamanhoY <= 250) {
+        cookie_esquerda.style.transform = `translateX(${180 -tamanhoY}px) scale(1.5)`;
+        cookie_direita.style.transform = `translateX(${-185 + tamanhoY}px) scale(1.5)`;
+        
+    }else if(tamanhoX <= 480 && tamanhoY <= 150) {//para mobile
+        cookie_esquerda.style.transform = `translateX(${104 - tamanhoY}px) scale(1.7)`;
+        cookie_direita.style.transform = `translateX(${-100 + tamanhoY}px) scale(1.7)`;
     }
+    //console.log(tamanhoY);
     
 });
+
 
 //cores: https://colorhunt.co/palette/ffecc0ffc29bf39f9fb95e82
