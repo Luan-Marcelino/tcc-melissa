f565bb81a145b3b3443c81b99a8b0e9e6503df84
Luan-Marcelino <luanmarcelinocontato@gmail.com>
2026-04-05 01:43:53 -0300

    carrossel improvisado

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ window.addEventListener('scroll', () => {
     const menu_fixo = document.querySelector('#home ul');
     const menu_opcoes = document.querySelectorAll('.menu-opcao');
     const tamanhoY = window.scrollY;
-    console.log(tamanhoY);
+    //console.log(tamanhoY);
     
     if(tamanhoY > 400) {
         menu_fixo.classList.add('pequeno');
@@ -21,4 +21,34 @@ window.addEventListener('scroll', () => {
 });
 
 
+//Carrossel de fotos do sobre nos
+
+const fotos = [
+    '/imagens/carrossel-img/carrossel1.avif',
+    '/imagens/carrossel-img/carrossel2.avif',
+    '/imagens/carrossel-img/carrossel3.jpg'
+]
+
+const container_fotos = document.querySelector('.img-slides');
+const imagensCriadas = document.querySelector('.img-slides img');
+
+let contador = 0;
+
+passarFotos(fotos);
+
+setInterval(() => {
+    passarFotos(fotos);
+    contador++;  
+}, 5000)
+
+function passarFotos(fotos) {
+    imagensCriadas.src = fotos[contador];
+    
+
+    if (contador >= fotos.length) {
+        contador = 0;
+        imagensCriadas.src = fotos[contador];
+        
+    }
+}
 //cores: https://colorhunt.co/palette/ffecc0ffc29bf39f9fb95e82
