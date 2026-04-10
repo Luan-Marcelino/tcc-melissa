2df80ef7c5282a3def872f6c1e6bed9199d500d9
Luan-Marcelino <luanmarcelinocontato@gmail.com>
2026-04-10 15:15:49 -0300

    Mudando imagem de capa

--- script/script.js
+++ script/script.js
@@ -1,54 +1,44 @@
-const body = document.querySelector('body');
-const cookie_esquerda = document.querySelector('.cookie-header-esquerda');
-const cookie_direita = document.querySelector('.cookie-header-direita');
-
-
+const menu_fixo = document.querySelector('#home ul');
+const menu_opcoes = document.querySelectorAll('.menu-opcao');
+const imgLogo = document.querySelector('.img-logo');
 
 window.addEventListener('scroll', () => {
-    const menu_fixo = document.querySelector('#home ul');
-    const menu_opcoes = document.querySelectorAll('.menu-opcao');
     const tamanhoY = window.scrollY;
-    //console.log(tamanhoY);
     
     if(tamanhoY > 400) {
+        imgLogo.classList.add('pequeno-logo');
         menu_fixo.classList.add('pequeno');
         menu_opcoes.forEach(opcoes => opcoes.classList.add('pequeno-opcao'));
     }else {
         menu_fixo.classList.remove('pequeno');
         menu_opcoes.forEach(opcoes => opcoes.classList.remove('pequeno-opcao'));
+        imgLogo.classList.remove('pequeno-logo');
     }
-    
 });
 
 
 //Carrossel de fotos do sobre nos
 
-const fotos = [
+const imgCarrossel = [
     '/imagens/carrossel-img/carrossel1.avif',
     '/imagens/carrossel-img/carrossel2.avif',
     '/imagens/carrossel-img/carrossel3.jpg'
 ]
 
 const container_fotos = document.querySelector('.img-slides');
-const imagensCriadas = document.querySelector('.img-slides img');
+const imagensSlides = document.querySelector('.img-slides img');
 
-let contador = 0;
+let indiceAtual = 0;
 
-passarFotos(fotos);
+carrossel();
 
 setInterval(() => {
-    passarFotos(fotos);
-    contador++;  
+    // É UM LOOP : 0 = (0 + 1) % 3 === 1 ---> 1 = (1 + 1) % 3 === 2 || 2 = (2 + 1) % 3 === 0 
+    indiceAtual = (indiceAtual + 1) % imgCarrossel.length;
+    carrossel();
 }, 5000)
 
-function passarFotos(fotos) {
-    imagensCriadas.src = fotos[contador];
-    
-
-    if (contador >= fotos.length) {
-        contador = 0;
-        imagensCriadas.src = fotos[contador];
-        
-    }
+function carrossel() {
+    imagensSlides.src = imgCarrossel[indiceAtual];
 }
 //cores: https://colorhunt.co/palette/ffecc0ffc29bf39f9fb95e82
