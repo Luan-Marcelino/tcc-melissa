734a103e64f9c384d65f6d424a248b0c504ac8e1
Luan-Marcelino <luanmarcelinocontato@gmail.com>
2026-04-17 16:19:59 -0300

    Adicionei carrossel em todos os textos

--- script/script.js
+++ script/script.js
@@ -24,20 +24,47 @@ const imgCarrossel = [
     '/imagens/carrossel-img/carrossel3.jpg'
 ]
 
+const txtSobreNos = [
+    
+    `<h2>Quem <span class="spam">Somos</span></h2>
+    <p>A Cookie Holic transforma um produto simples em uma experiência de qualidade.</p>
+    <p>Receitas equilibradas, ingredientes selecionados e processos bem definidos garantem consistência e padrão.</p>
+    <p>Cookies desenvolvidos para entregar sabor, textura e apresentação alinhados a quem valoriza qualidade.</p>`
+    ,
+    
+    `<h2>Nossa <span class="spam">Visão</span></h2>
+    <p>Ser referência em cookies artesanais, com foco em padronização e posicionamento profissional</p>`
+    ,
+    
+    `<h2>Nossos <span class="spam">Valores</span></h2>
+    <p>Qualidade, padronização, compromisso com o cliente e melhoria contínua em todos os processos.</p>`
+    
+]
+
 const container_fotos = document.querySelector('.img-slides');
 const imagensSlides = document.querySelector('.img-slides img');
-
-let indiceAtual = 0;
+const container_info = document.querySelector('.info');
+let indiceAtualImg = 0;
+let indiceAtualTxt = 0;
 
 carrossel();
 
 setInterval(() => {
     // É UM LOOP : 0 = (0 + 1) % 3 === 1 ---> 1 = (1 + 1) % 3 === 2 || 2 = (2 + 1) % 3 === 0 
-    indiceAtual = (indiceAtual + 1) % imgCarrossel.length;
+    indiceAtualImg = (indiceAtualImg + 1) % imgCarrossel.length;
     carrossel();
-}, 5000)
+}, 13000)
+
+setInterval(() => {
+    indiceAtualTxt = (indiceAtualTxt + 1) % txtSobreNos.length;
+    carrossel();
+}, 13000)
 
 function carrossel() {
-    imagensSlides.src = imgCarrossel[indiceAtual];
+    imagensSlides.src = imgCarrossel[indiceAtualImg];
+    container_info.innerHTML = txtSobreNos[indiceAtualTxt];
+    
 }
+
+
 //cores: https://colorhunt.co/palette/ffecc0ffc29bf39f9fb95e82
